db58bc590a792554a1dd370e72a58d6aa7c97182
liuzengyun <zenyon1999@gmail.com>
2020-11-25 16:21:42 +0800

    修改2-1，指导书1图

--- code/lab2/t1.m
+++ code/lab2/t1.m
@@ -1,29 +1,26 @@
-fs=100; %调制信号mt的采样频率
-fc=10;   %载波信号频率
-df=0.1;
-f=0:df:600;
-h=1/fs;
-t=0:h:10;
-mt=1.414*cos(2*pi*t);
-st=mt.*cos(2*pi*fc.*t);
-SFs=sig_spec(st,t,h,f);
+fc=10;  %载波信号频率
+df=0.1; %频域间隔
+f=0:df:1000;%频域横轴
+dt=0.01;%时域时间间隔
+t=0:dt:10;%时域横轴
+
+
+mt=sqrt(2)*cos(2*pi*t);%消息信号
+st=mt.*cos(2*pi*fc.*t);%调制信号
+SF=sig_spec(st,t,dt,f);%调用傅里叶变换函数，信号与系统用过
+
 
 subplot(2,1,1);
 plot(t,st);
-xlabel('t(s)');
+xlabel('t');
 ylabel('st');
-title('DSB信号时域波形');
+title('DSB信号的时域波形');
 grid on;
 
 subplot(2,1,2);
-%plot(f,SFs);
-plot(f,abs(SFs).^2/t(end));
-xlabel('f(HZ)');
+plot(f,abs(SF).^2/t(end));
+xlabel('f');
 ylabel('Ps');
-title('DSB信号功率谱密度');
+title('DSB信号的功率谱密度');
 axis([5 15 0 2]);
 grid on;
-
-
-
-
